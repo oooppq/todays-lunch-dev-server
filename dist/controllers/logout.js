aba12ce07a46fe4caa64283feaf2b8c9a5ebfe5d
DaegyunOh <oooppq@naver.com>
2023-08-17 17:16:47 +0900

    logout res fix

--- dist/controllers/logout.js
+++ dist/controllers/logout.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.logout = void 0;
 const RES = true;
 const logout = (req, res, next) => {
-    res.json(RES);
+    res.status(200).json();
 };
 exports.logout = logout;
